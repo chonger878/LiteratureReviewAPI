c1114f211c0333fe904c0e225776e62ce4199eb6
efhiii <efhiii2000@gmail.com>
2020-07-07 00:00:54 -0700

    Tree traversal

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+const CitationMinimum = 2000;
+const MaximumArticles = 5;
+
 const scholarly = require("scholarly");
 /*
 // To search for a specific topic
@@ -72,11 +75,11 @@ function sleep(ms) {
  * getChildrenArticles - Retrieves the most relevent articles that cite a given article
  *
  * @param  {string} url search url
- * @param  {number} [citationMinimum = 10] minimum citations
- * @param  {number} [maximumArticles = 20] maximum number of articles to return
+ * @param  {number} [citationMinimum] minimum citations
+ * @param  {number} [maximumArticles] maximum number of articles to return
  * @return {[Article Array Promise]} resolves with a list of found articles
  */
-async function getArticles(url, citationMinimum = 10, maximumArticles = 20) {
+async function getArticles(url, citationMinimum = CitationMinimum, maximumArticles = MaximumArticles) {
   let lowest = Infinity;
   let children = [];
   let page = 0;
@@ -85,32 +88,45 @@ async function getArticles(url, citationMinimum = 10, maximumArticles = 20) {
     let ms = Math.random() * 10000 + Math.random() * 4000 + Math.random() * 4000 + 10000;
     console.log('sleep for ' + (ms / 10 >> 0) / 100 + ' seconds');
     await sleep(ms);
+    console.clear();
+    console.log(treeVis);
+    console.log(treeVisB);
 
     let currentURL = url + `&as_vis=1&as_sdt=1,5${page?`&start=${page}`:''}`;
     console.log('- query: ' + currentURL);
 
     //try multiple times in the event of an error, like ECONNREFUSED or ETIMEDOUT
-    let done = false,
+    let done = 0,
       newChildren;
-    while(!done) {
+    while(done < 5) {
       try {
         newChildren = await scholarly.search(currentURL);
-        done = true;
+        done = Infinity;
       } catch (e) {
-        console.log(`error: ${e.code}\ntrying again . . .`);
+        done++;
+        if(done < 5) {
+          console.log(`error: ${e}\ntrying again . . . ` + done);
+        }
       }
+    }
+    if(done !== Infinity) {
+      throw 'Too many errors';
     }
 
     page += 10;
     children = children.concat(newChildren); //concat children to output array
 
-    lowest = newChildren[newChildren.length - 1].numCitations;
+    for(let i = 0; i < newChildren.length; i++) {
+      lowest = Math.min(lowest, newChildren[i].numCitations);
+    }
     if(newChildren.length <= 0) {
       lowest = 0;
     }
     console.log(children.length + '/' + maximumArticles); //progress over maximum articles
   }
   console.log(children.length + ' hits'); //total articles returned
+
+  console.log(children.map(prettyMap).join('\n'));
   return children;
 }
 
@@ -118,12 +134,25 @@ async function getArticles(url, citationMinimum = 10, maximumArticles = 20) {
  * getChildrenArticles - Retrieves the most relevent articles that cite a given article
  *
  * @param  {Article} article original article
- * @param  {number} [citationMinimum = 10] minimum citations
- * @param  {number} [maximumArticles = 20] maximum number of articles to return
+ * @param  {number} [citationMinimum] minimum citations
+ * @param  {number} [maximumArticles] maximum number of articles to return
  * @return {[Article Array Promise]} resolves with a list of children articles
  */
-function getChildrenArticles(article, citationMinimum = 10, maximumArticles = 20) {
+function getChildrenArticles(article, citationMinimum = CitationMinimum, maximumArticles = MaximumArticles) {
   let url = article.citationUrl.replace('http://scholar.google.com/scholar?', '&');
+  return getArticles(url, citationMinimum, maximumArticles);
+}
+
+/**
+ * getNeighborArticles - Retrieves the most relevent articles to a given article
+ *
+ * @param  {Article} article original article
+ * @param  {number} [citationMinimum] minimum citations
+ * @param  {number} [maximumArticles] maximum number of articles to return
+ * @return {[Article Array Promise]} resolves with a list of children articles
+ */
+function getNeighborArticles(article, citationMinimum = CitationMinimum, maximumArticles = MaximumArticles) {
+  let url = article.relatedUrl.replace('http://scholar.google.com/scholar?q=', '');
   return getArticles(url, citationMinimum, maximumArticles);
 }
 
@@ -132,11 +161,11 @@ function getChildrenArticles(article, citationMinimum = 10, maximumArticles = 20
  * @param {string} searchTerm main search term within blurb/title
  * @param {[string]} [authors] author(s) name(s)
  * @param {number} [year] publication year
- * @param  {number} [citationMinimum = 10] minimum citations
- * @param  {number} [maximumArticles = 20] maximum number of articles to return
+ * @param  {number} [citationMinimum] minimum citations
+ * @param  {number} [maximumArticles] maximum number of articles to return
  * @return {[Article Array Promise]} Array of articles found with the given search terms
  */
-function searchArticles(searchTerm, names, year, citationMinimum = 10, maximumArticles = 20) {
+function searchArticles(searchTerm, names, year, citationMinimum = CitationMinimum, maximumArticles = MaximumArticles) {
   console.log('Searching ' + searchTerm +
     (names && names.length > 0 ? `${names.join(' ').replace('','')}` : '') +
     (year ? ` from ${year}` : ''));
@@ -145,6 +174,112 @@ function searchArticles(searchTerm, names, year, citationMinimum = 10, maximumAr
     (year ? ` &as_ylo=${year}&as_yhi=${year}` : '') +
     '&as_vis=1&as_sdt=1,5';
   return getArticles(url);
+}
+
+
+/**
+ * articleID - Get an article's unique ID
+ *
+ * @param  {Article} article article object
+ * @return {string} article ID
+ */
+function getArticleID(article) {
+  return article && article.hasOwnProperty('citationUrl') ? article.citationUrl.match(/[0-9]+/)[0] : 'null';
+}
+
+let treeVis=[];
+let treeVisB=[];
+
+async function branch(steps, article, parent, searched, searchedBranch) {
+  let parentID = parent ? getArticleID(parent) : false;
+  let articleID = getArticleID(article);
+  if(parentID && searched.hasOwnProperty(parentID + '->' + articleID)) {
+    return;
+  }
+  if(parent) {
+    searched[parentID + '->' + articleID] = true;
+  }
+  if(searchedBranch.hasOwnProperty(articleID)) {
+    return;
+  }
+  searchedBranch[articleID] = article;
+
+  treeVis[steps]++;
+  if(article.numCitations < CitationMinimum) { return; }
+  if(steps <= 0) { return; }
+  treeVisB[steps]++;
+
+  let children = await getChildrenArticles(article);
+  for(let i = 0; i < children.length; i++) {
+    await branch(steps - 1, children[i], article, searched, searchedBranch);
+  }
+
+  if(steps === 1){return;}
+
+  let neighbors = await getNeighborArticles(article);
+  for(let i = 0; i < neighbors.length; i++) {
+    await branch(steps - 1, neighbors[i], false, searched, searchedBranch);
+  }
+  //printTree(searched,searchedBranch);
+}
+
+
+/**
+ * addSlashes - Escapes a string
+ *
+ * @param  {string} str original string
+ * @return {string} escaped string
+ */
+function addSlashes( str ) {
+  //https://locutus.io/php/strings/addslashes/
+  return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+
+/**
+ * printTree - Prints Graphviz code from a tree
+ *
+ * @param  {type} searched list of searched article pairs
+ * @param  {type} searchedBranch list of searched articles
+ */
+function printTree(searched, searchedBranch) {
+  let treeText = '';
+  for(let node in searchedBranch) {
+    let cites = searchedBranch[node].numCitations / 10 >> 0;
+    cites = -1 / Math.pow(2, Math.log(searchedBranch[node].numCitations / 1000) / Math.log(10)) + 1;
+    treeText += 'node [color="#' +
+      ((255 - (cites * 255 >> 0)).toString(16).padStart(2, '0')) +
+      '00' +
+      ((cites * 255 >> 0).toString(16).padStart(2, '0')) + '" label ="' + addSlashes(searchedBranch[node].title) + '" weight=' + (50 * cites >> 0) + ' tooltip="' + searchedBranch[node].numCitations + '"];\n' + node + ';\n';
+  }
+  for(var node in searched) {
+    treeText += node + ';\n';
+  }
+
+  console.log('\n\ndigraph G {\nnode [style=filled fontcolor=white];\n');
+  console.log(treeText);
+  console.log('}\n\n');
+}
+
+//takes the same arguments as searchArticles
+async function buildArticleTree(steps, args) {
+  treeVis=[];
+  treeVisB=[];
+  for(let i=0;i<steps;i++){
+    treeVis.push(0);
+    treeVisB.push(0);
+  }
+
+  let root = await searchArticles(...args);
+  let searched = {};
+  let searchedBranch = {};
+  try {
+    for(let i = 0; i < root.length; i++) {
+      await branch(steps - 1, root[i], false, searched, searchedBranch);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  printTree(searched, searchedBranch);
 }
 
 /**
@@ -185,12 +320,13 @@ BgCyan = "\x1b[46m"
 BgWhite = "\x1b[47m"
 */
 function prettyMap(article) {
-  return `\x1b[33m${(''+article.numCitations).padStart(7,' ')}\x1b[36m - ${article.year}\x1b[32m ${article.title}\x1b[0m`;
+  return `\x1b[33m${(''+article.numCitations).padStart(7,' ')}\x1b[36m - ${article.year}\x1b[32m ${article.title}\x1b[0m ${getArticleID(article)}`;
 }
 
 async function main() {
-  const art = await searchArticles('biology');
-  console.log(art.map(prettyMap).join('\n'));
+  await buildArticleTree(3, ['information theory']);
+  //const art = await searchArticles('astrology', undefined, undefined, 10, 10);
+  //console.log(art.map(prettyMap).join('\n'));
   //console.log(art[0]);
 
   //const chil = await getChildrenArticles(art[0]);
