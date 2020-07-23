7c137641c61a51ae9f243ad4c31efe6a1af68176
EricaC <Char-wae.Chong@seatlecolleges.edu>
2020-07-23 00:41:02 -0700

    added childPaper as a data object

--- public/MongoDB Functions/writeDataFunction.js
+++ public/MongoDB Functions/writeDataFunction.js
@@ -3,11 +3,12 @@ let mongoose = require('mongoose');
 var Schema = mongoose.Schema;
  
 const ScholarlyArticlesSchema =  new Schema({
+
+    //Null string?
     _id: String,
-    articleName: String,
-    articleAuthor: String
-    
-    
+
+    //Saves information about child paper into an array
+    childPaper: [{articleName:String, articleAuthor:String, articleUrl:String}]   
 })
 
 const articlesModel = mongoose.model('Scholarly Articles', ScholarlyArticlesSchema);
@@ -16,5 +17,8 @@ const articleListing = new articlesModel();
 
 await articleListing.save();
 
+//Article id becomes a concatenated string of the article name and author
 articleListing._id = articleName + "_" + articleAuthor;
+
+
 await articleListing.save();
